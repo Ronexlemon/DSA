aa1437ba0ec9bea99a428cff563d7883fe2914b0
RonexLemon <ronexondimu@gmail.com>
2024-08-22 13:10:27 +0300

    addition

---
+++ Go/Basic-Maths/addition/add.go
@@ -0,0 +1,32 @@
+package addition
+
+import "fmt"
+
+func p_sum(a int, b int)int{
+	return a+b
+}
+
+//sum of two negative numbers
+
+func n_Sum(a int,b int)int{
+	return a+b
+
+}
+
+//sum for a positive number and negative
+func p_n_sum(a int,b int)int{
+	return a+b
+
+}
+
+func Add(){
+	a:=10
+	b:=20
+	c:=-10
+	d:=-20
+	fmt.Println("Sum of two positive numbers:",p_sum(a,b))
+	fmt.Println("Sum of two negative numbers:",n_Sum(c,d))
+	fmt.Println("sum of a positive and a negative number",p_n_sum(a,c))
+
+	
+}
---
+++ Go/Basic-Maths/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"gobasicmaths/addition"
+)
+
+
+func main(){
+	//basic maths
+	fmt.Println("Basic Maths .................*****************......................")
+	addition.Add()
+}

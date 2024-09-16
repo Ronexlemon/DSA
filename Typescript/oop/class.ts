0a8dcfe9cc88b26281c28ab5f0666d1d817ec849
RonexLemon <ronexondimu@gmail.com>
2024-09-16 21:30:49 +0300

    getters & setters

--- Typescript/oop/class.ts
+++ Typescript/oop/class.ts
@@ -15,8 +15,25 @@ class Point{
     }
 }
 
+class Getters_Setters{
+    _length:number=10
+
+    get length():number{
+        return this._length;
+    }
+    set length(newNumber:number){
+        this._length = newNumber
+    }
+
+}
 const p = new Point(10,20)
 
 
 
 console.log(p)
+console.log("getters and setters .......")
+const gs = new Getters_Setters()
+gs._length = 100
+gs.length =20
+
+console.log(gs.length)

9c51bd0add261b145878e650c56c79d2fef61b2e
RonexLemon <ronexondimu@gmail.com>
2024-09-16 21:48:28 +0300

    overriding

--- Typescript/oop/class.ts
+++ Typescript/oop/class.ts
@@ -26,7 +26,56 @@ class Getters_Setters{
     }
 
 }
+
+//class heritage
+
+interface Pingable{
+    ping():string
+}
+interface Checkable{
+    check(name:string):boolean
+}
+class Ping_it implements Pingable {
+    ping():string{
+        return "pinging ..."
+    }
+
+}
+class CheckOut implements Checkable{
+    check(s:any){
+        return  s.toLowerCase() === "ok";
+
+    }
+}
+
+//inheritance
+class Person{
+    talk(){
+        console.log("Hello i can talk")
+    }
+}
+class John extends Person{
+    name(){
+        console.log("Hello my name is John")
+    }
+}
+
+//overriding
+class Mary extends John{
+    talk(newMessage?:string){
+        if(newMessage === undefined){
+            super.talk()
+        }else{
+            console.log(newMessage)
+        }
+      
+
+    }
+}
 const p = new Point(10,20)
+
+
+
 
 
 
@@ -37,3 +86,18 @@ gs._length = 100
 gs.length =20
 
 console.log(gs.length)
+
+console.log("implements.......")
+const pn = new Ping_it()
+const ch = new CheckOut()
+
+console.log("Yes have checkit",ch.check("helloYOWWW"))
+console.log(pn.ping())  //output: pinging ...
+//inheritance
+const per = new John()
+per.name()
+per.talk()
+//overriding
+const mar = new Mary()
+mar.talk()
+mar.talk("Yes i can talk tooo")

d9153f84945178b6fe448df8e2e38f12ac1aa2f5
RonexLemon <ronexondimu@gmail.com>
2024-09-17 07:38:17 +0300

    generic constraint

--- Typescript/Type/generic.ts
+++ Typescript/Type/generic.ts
@@ -21,9 +21,34 @@ const  Identity = <Type>(arg: Type): Type=> {
   interface GenericIdentity<Type>{
     (arg:Type):Type
   }
+  interface LengthWise{
+    length: number
+  }
   let myGeneric: GenericTypeFn = Identity
   let myGeneric2: GenericIdentity<number> = Identity
   let myGeneric3: GenericIdentity<string> = Identity
   console.log(myGeneric3("hello"))
   console.log(myGeneric2(40))
   console.log(myGeneric(10))
+
+  //generic classes
+  console.log("Generic classes ............................")
+  class GenericClass<Type>{
+    value:Type
+    constructor(value:Type){
+        this.value=value
+        }
+        getValue():Type{
+            return this.value
+            }
+  }
+
+  const gc = new GenericClass<number>(45)
+  console.log(gc.getValue())
+  //Generic contraints
+  console.log("Generic contraints ............................")
+  const LoggingIdentity = <Type extends LengthWise>(arg:Type):Type=>{
+    console.log(arg.length)
+    return arg
+  }
+  console.log(LoggingIdentity("hello"))

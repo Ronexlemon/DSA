283b40bf56fa925ee4e534d154963c19d933be3a
RonexLemon <ronexondimu@gmail.com>
2024-09-20 09:03:11 +0300

    concat()

--- Typescript/arrays/array.ts
+++ Typescript/arrays/array.ts
@@ -1,6 +1,7 @@
 
 const newArr = new Array([{"name":"rone"}])
 const ArrayNew = ["Banana","Golang","SQL","DATABASE"]
+const ArrayNew2 = ["Orange","MongoDb","Prisma","Typescript"]
 import { objectsTokens } from "../objects/types"
 
 //Array methods and properties
@@ -15,11 +16,22 @@ import { objectsTokens } from "../objects/types"
 
 //get elemens at index 0
 
-const itemAtIndex0 = objectsTokens[0]
+const itemAtIndex0 = objectsTokens.flatMap((items)=> items["CUSD"])
 
 
+//concat()
+/**
+ * Concatenates (joins) two or more arrays
+ * returns a new array, containing the joined arrays
+ *  does not change the original arrays
+ */
+const anotherArray = ArrayNew.concat(ArrayNew2)
+const finalArray = anotherArray.concat(anotherArray)
 
 
 console.log(newArr)
 console.log(itemAtIndex0)
+console.log(anotherArray)
+console.log(finalArray)
+
 

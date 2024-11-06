fb3e5eac9ed47eb36a2179824631262c815c04bb
RonexLemon <ronexondimu@gmail.com>
2024-11-06 11:47:21 +0300

    data:obj

---
+++ Typescript/arrays/methods/data.ts
@@ -0,0 +1,102 @@
+export const dataNew = [
+    {
+        "name": "Bmw",
+        "make": {
+            "year": 2024,
+            "model": "X5",
+            "country": "Germany"
+        },
+        "price": 50000,
+        "color": "Red"
+    },
+    {
+        "name": "Audi",
+        "make": {
+            "year": 2023,
+            "model": "Q7",
+            "country": "Germany"
+        },
+        "price": 55000,
+        "color": "Blue"
+    },
+    {
+        "name": "Mercedes",
+        "make": {
+            "year": 2022,
+            "model": "GLC",
+            "country": "Germany"
+        },
+        "price": 60000,
+        "color": "Black"
+    },
+    {
+        "name": "Toyota",
+        "make": {
+            "year": 2024,
+            "model": "Highlander",
+            "country": "Japan"
+        },
+        "price": 45000,
+        "color": "White"
+    },
+    {
+        "name": "Ford",
+        "make": {
+            "year": 2023,
+            "model": "Explorer",
+            "country": "USA"
+        },
+        "price": 47000,
+        "color": "Gray"
+    },
+    {
+        "name": "Honda",
+        "make": {
+            "year": 2022,
+            "model": "CR-V",
+            "country": "Japan"
+        },
+        "price": 40000,
+        "color": "Silver"
+    },
+    {
+        "name": "Tesla",
+        "make": {
+            "year": 2024,
+            "model": "Model X",
+            "country": "USA"
+        },
+        "price": 80000,
+        "color": "White"
+    },
+    {
+        "name": "Chevrolet",
+        "make": {
+            "year": 2023,
+            "model": "Tahoe",
+            "country": "USA"
+        },
+        "price": 52000,
+        "color": "Black"
+    },
+    {
+        "name": "Nissan",
+        "make": {
+            "year": 2022,
+            "model": "Pathfinder",
+            "country": "Japan"
+        },
+        "price": 43000,
+        "color": "Blue"
+    },
+    {
+        "name": "Lexus",
+        "make": {
+            "year": 2024,
+            "model": "RX",
+            "country": "Japan"
+        },
+        "price": 60000,
+        "color": "Red"
+    }
+];

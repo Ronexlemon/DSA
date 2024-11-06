7c74f61a9c70a1434892f68dd9e3809916d0774e
RonexLemon <ronexondimu@gmail.com>
2024-11-06 13:44:56 +0300

    map

--- Typescript/arrays/methods/concat.ts
+++ Typescript/arrays/methods/concat.ts
@@ -2,6 +2,7 @@ import { dataNew } from "./data";
 
 const Concat =()=>{
     const dataDouble = dataNew.concat(...dataNew)
+    
     console.log("new combine data",dataDouble.length)
 }
 (()=>{
---
+++ Typescript/arrays/methods/map.ts
@@ -0,0 +1,14 @@
+import { dataNew } from "./data";
+
+
+const Map =()=>{
+    dataNew.map((element)=>{
+        console.log(element);
+
+    })
+}
+
+(()=>{
+    Map()
+
+})()

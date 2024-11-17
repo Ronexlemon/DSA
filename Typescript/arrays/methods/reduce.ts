6b51113c63d4170370374becbb512183803a89a3
RonexLemon <ronexondimu@gmail.com>
2024-11-17 16:09:36 +0300

    object:red

--- Typescript/arrays/methods/reduce.ts
+++ Typescript/arrays/methods/reduce.ts
@@ -55,6 +55,18 @@ const Re =()=>{
     },[] as string[])
     console.log("new array of names",carNamesArray)
 }
+const ReducrNAme =()=>{
+    const totalPrice = dataNew.reduce((acc,cur)=>{
+        return acc + cur.price
+        },0)
+        console.log("totalPrice",totalPrice)
+        //return an array of all car models
+        const carNamesArray = dataNew.reduce((acc,cur)=>{
+            return acc.concat(cur.name);
+            },[] as string[])
+            console.log("new array of names",carNamesArray)
+            }
+
 (()=>{
     Reduce()
 })()

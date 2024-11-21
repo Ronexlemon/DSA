8fd08e73d43a4a86f904573cb1e850b8ef437fd6
RonexLemon <ronexondimu@gmail.com>
2024-11-21 19:59:45 +0300

    fib

--- Go/DSA/fib/fib.go
+++ Go/DSA/fib/fib.go
@@ -13,7 +13,20 @@ func Fib(n int)int{
 			return Fib(n-1)+Fib(n-2)
 		
 }
+func Fib2(num int){
+	fib :=make([]int,num)
+	fib[0] =0
+	fib[1] =1
+	for i:=2; i<num;i++{
+		fib[i] = fib[i-1] +fib[i-2]
+		
+	}
+	for _,nums :=range fib{
+		fmt.Print(nums)
+	}
+}
 func main(){
 	num:= 20
 	fmt.Println(Fib(num))
+	Fib2(num)
 }
